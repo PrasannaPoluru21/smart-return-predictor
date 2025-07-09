cf9c5d5c1609fc0e511f6b53de6570b462156265
PrasannaPoluru21 <prasannapolurums@gmail.com>
2025-07-09 17:53:29 -0600

    Updating EDA + Feature Selection

--- notebooks/eda.py
+++ notebooks/eda.py
@@ -1,12 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
+from sklearn.model_selection import train_test_split
 
-# Load dataset
+#loading the datasets
 customers_df = pd.read_csv('../data/olist_customers_dataset.csv')
-# print(customers_df.shape)
-# customers_df.head()
-
 geolocation_df = pd.read_csv('../data/olist_geolocation_dataset.csv')
 order_items_df = pd.read_csv('../data/olist_order_items_dataset.csv')
 order_payments_df = pd.read_csv('../data/olist_order_payments_dataset.csv')
@@ -26,8 +25,18 @@ print(products_df.columns)
 print(sellers_df.columns)
 print(product_translation_df.columns)
 
-updated_orders_payments_df = pd.merge(orders_df, order_payments_df, on='order_id', how='left')
+#Merging all the tables according to the relevance
+updated_orders_payments_df=pd.merge(orders_df, order_payments_df, on = 'order_id', how ='left' )
+
+updated_orders_payments_df=pd.merge(orders_df, order_payments_df, on = 'order_id', how ='left' )
+
+updated_orders_payments_df=pd.merge(orders_df, order_payments_df, on = 'order_id', how ='left' )
+
 updated_products_order_items_df=pd.merge(order_items_df, products_df, on='product_id', how='left' )
+
+updated_orders_payment_reviews_df = pd.merge(updated_orders_payments_df, order_reviews_df, on='order_id', how='left')
+
+# sellers_geolocation_df=pd.merge(sellers_df, geolocation_df, left_on='seller_zip_code_prefix', right_on='geolocation_zip_code_prefix', how='left')
 
 # Take average lat/lng for each zip code prefix
 geo_grouped = geolocation_df.groupby('geolocation_zip_code_prefix').agg({
@@ -40,8 +49,6 @@ sellers_geolocation_df = pd.merge(sellers_df, geo_grouped, left_on='seller_zip_c
 
 updated_sellers_geolocation_products_order_items_df= pd.merge(sellers_geolocation_df, updated_products_order_items_df, on='seller_id', how='right')
 
-updated_orders_payment_reviews_df = pd.merge(updated_orders_payments_df, order_reviews_df, on='order_id', how='left')
-
 updated_orders_payment_reviews_customers_df=pd.merge(updated_orders_payment_reviews_df, customers_df, on='customer_id', how='left')
 
 combined_df=pd.merge(updated_orders_payment_reviews_customers_df, updated_sellers_geolocation_products_order_items_df, on='order_id', how='left')
@@ -51,14 +58,9 @@ combined_df['order_status'].value_counts()
 #Adding target Variable
 combined_df['is_returned'] = combined_df['order_status'].apply(lambda x: 1 if x == 'canceled' else 0)
 
-#for reference
-# df.drop('column_name', axis=1, inplace=True)
-# df.drop(['col1', 'col2'], axis=1, inplace=True)
-# df.rename(columns={'old_name': 'new_name'}, inplace=True)
-# df.rename(columns={'old1': 'new1', 'old2': 'new2'}, inplace=True)
-
 combined_df=combined_df[['customer_city', 'customer_state','seller_city', 'seller_state','price',
 'product_category_name','product_weight_g','product_length_cm', 'product_height_cm', 'product_width_cm', 'product_photos_qty','freight_value', 'order_approved_at','order_delivered_customer_date', 'order_estimated_delivery_date', 'review_score','payment_value','payment_type', 'payment_installments','is_returned']]
+
 
 final_df=pd.merge(combined_df,product_translation_df, on='product_category_name', how='left')
 
@@ -84,33 +86,62 @@ final_df = final_df.drop(columns=[
     'order_approved_at', 'order_delivered_customer_date', 'order_estimated_delivery_date'
 ])
 
-# final_df = pd.get_dummies(final_df, columns=['payment_type'])
 final_df = pd.get_dummies(final_df, columns=['payment_type', 'product_category', 'customer_state', 'seller_state'], drop_first=True)
 
 final_df = final_df.dropna()
+
 final_df = final_df.reset_index(drop=True)
 
-#Train-Test Split
-from sklearn.model_selection import train_test_split
-
-final_df[['is_returned']].head()
-
-final_df.columns
-
 non_numeric = final_df.select_dtypes(include='object').columns
-print(non_numeric)
 
 final_df=final_df.drop(columns=['customer_city', 'seller_city'])
 
-#setting the gonna split data with feature and target columns
-X = final_df.drop(columns=['is_returned'])
-y=  final_df['is_returned']
+#Quick_look into data
+print(final_df.shape)
+print(final_df.info())
 
-#Splitting the data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+#checking for missing values
+print(final_df.isna().sum().sort_values(ascending=False))
 
-# used stratify here to maintain even class distribution as there might be less no of returned items
+#understanding target variable
+print(final_df['is_returned'].value_counts(normalize=True).plot(kind='bar', title='Return Rate'))
+
+# As here this column is highly imbalanced, we will use stratified sampling or resampling later
+
+print(final_df[['price', 'freight_value', 'review_score', 'delivery_delay']].describe())
+
+print(final_df[['price', 'freight_value', 'delivery_delay']].hist(bins=30, figsize=(12, 6)))
+
+# Price is highly skewed, have to consider using log-transforming or clipping outliers
+# freight_value is right skewed, most shipping costs are under $100, can consider standardising or normalising
+# Delivery_delay seems to be normally distributed, but centered around a negative mean, but this looks highly predictive, keep as it is
+# In review_score, most are 4 or 5, can one-hot encode or bucket into groups
+
+#Applying log transform on price 
+#As this is feature enfgineering step we can do before splitting the data
+final_df['log_price'] = np.log1p(final_df['price'])  # AS log1p handles 0 safely
+
+# Bucket review_score
+final_df['review_bucket'] = final_df['review_score'].apply(
+    lambda x: 'low' if x <= 2 else 'medium' if x == 3 else 'high')
+
+print(final_df[['log_price', 'freight_value', 'delivery_delay', 'review_bucket','review_score' ]].head(5))
+
+print(final_df.corr(numeric_only=True)['is_returned'].sort_values(ascending=False))
+
+print(final_df['payment_type_not_defined'].value_counts())
+
+# for col in final_df.columns:
+#     print(col)
+
+# As with .corr(), we got the correlation of numeric values, we are using Cramers V for checking correlation with categorical values
 
 final_df.to_csv("/Users/lakshmiprasannapoluru/Desktop/smart-return-predictor/data/final_df.csv", index=False)
 
-print('Execution done!')
+#Finalising the features for base model
+features = ['is_returned','delivery_delay','payment_value','price','customer_state_SP',
+           'product_category_bed_bath_table','review_score','payment_type_not_defined' ]
+
+#Setting feature and target columns
+X = final_df[features].drop(columns=['is_returned'])
+y = final_df['is_returned']
